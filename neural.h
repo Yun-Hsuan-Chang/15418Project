5cc009b14a7772eee23e01edf550889895232f0e
Yun-Hsuan-Chang <55062705+Yun-Hsuan-Chang@users.noreply.github.com>
2020-05-16 02:56:26 -0400

    Update neural.h

--- neural.h
+++ neural.h
@@ -1,27 +1,3 @@
-// #include "model.h"
-
-// struct Model_t;
-
-// class NNModel{
-
-// public:
-//     virtual ~NNModel(){ };
-    
-//     virtual void load_model(Model_t& model) = 0;
-    
-//     virtual int forward(double* data, double* label) = 0;
-    
-//     virtual void backward() = 0;
-    
-//     virtual void sgd() = 0;
-
-//     virtual void forward_backward(double* data, double* label) = 0;
-
-//     virtual void forward_backward_sgd(Model_t* model) = 0;
-
-//     virtual void printTime() = 0;
-// };
-
 #include "model.h"
 
 struct Model_t;
